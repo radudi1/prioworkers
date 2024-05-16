e165db068584b3cd91b87326923397687f8a38fb
radudi1 <gthb@radudi.com>
2024-05-16 14:52:26 +0300

    Complete rewrite using atomic primitives wherever possible

--- prioworkers.go
+++ prioworkers.go
@@ -1,149 +1,122 @@
 package prioworkers
 
+import (
+	"sync/atomic"
+)
+
+const numPriorities = 100
+
 type PrioworkersOptions struct {
-	MaxBlockedWorkers        int // if number of blocked workers exceeds this limit a worker will be spinned even if priority doesn't allow it
-	LowPrioSpinCnt           int // number of low priority workers that will be spinned at the same time when priority allows
+	MaxBlockedWorkers        int64 // if number of blocked workers exceeds this limit a worker will be spinned even if priority doesn't allow it
+	LowPrioSpinCnt           int64 // number of low priority workers that will be spinned at the same time when priority allows
 	EnforceSpinCntOnHighPrio bool
 }
 
 type State struct {
 	// current state
-	WorkStartChanLen         int
-	WorkEndChanLen           int
-	RunningWorkersPerPrioCnt [100]int
-	WaitingWorkersPerPrioCnt [100]int
+	RunningWorkersPerPrioCnt [numPriorities]int64
+	WaitingWorkersPerPrioCnt [numPriorities]int64
 	// all time maximums
-	MaxEverWorkChanLen     int
-	MaxEverRunningSamePrio int
-	MaxEverWaitingSamePrio int
+	MaxEverRunningSamePrio int64
+	MaxEverWaitingSamePrio int64
 }
 
 var Options PrioworkersOptions
 
-var workStartChan chan int
-var workEndChan chan int
-var workerChans [100]chan struct{} // a channel for each priority; workers will block on reading from it
-var chanBuffSize int               // size of internal channel buffers;
-var getStateChan chan struct{}
-var stateChan chan State
-
-func Init(chanBuffSize int, options *PrioworkersOptions) {
-	Options = *options
-	workStartChan = make(chan int, chanBuffSize)
-	workEndChan = make(chan int, chanBuffSize)
-	for i := range workerChans {
-		workerChans[i] = make(chan struct{})
-	}
-	getStateChan = make(chan struct{})
-	stateChan = make(chan State)
-	go workerCoordinator()
+type prioCounters struct {
+	prioCnt [numPriorities]atomic.Int64
+	maxEver atomic.Int64
 }
 
-func WorkStart(prio int) int {
-	workStartChan <- prio
-	<-workerChans[prio]
+var prioRunningCnt prioCounters
+var prioWaitingCnt prioCounters
+var curRunningPrio atomic.Int64
+var workerChans [numPriorities]chan struct{}
+
+func Init(options *PrioworkersOptions) {
+	Options = *options
+	for i := 0; i < numPriorities; i++ {
+		workerChans[i] = make(chan struct{})
+	}
+}
+
+func WorkStart(prio int64) int64 {
+	// if work prio is at least current running prio we can start immediately - no further processing or storing needs to be done
+	if prio >= curRunningPrio.Load() {
+		curRunningPrio.Store(prio)
+		prioRunningCnt.inc(prio)
+	} else { // worker with higher priorities are running and current worker should wait (unless limit is exceeded)
+		// if the number of waiting threads hit the maxBlockedWorkers limit we let it run immediately; otherwise we add it to waiting queue
+		if Options.MaxBlockedWorkers > 0 && prioWaitingCnt.get(prio) >= Options.MaxBlockedWorkers {
+			prioRunningCnt.inc(prio)
+		} else {
+			prioWaitingCnt.inc(prio)
+			<-workerChans[prio] // block waiting for other workers to finish
+			// worker can now start and update state accordingly
+			prioWaitingCnt.dec(prio)
+			prioRunningCnt.inc(prio)
+		}
+	}
 	return prio
 }
 
-func WorkEnd(prio int) {
-	workEndChan <- prio
+func WorkEnd(prio int64) {
+	prioRunningCnt.dec(prio)
+	// if current running priority is higher than this worker priority there's nothing else to do
+	if curRunningPrio.Load() > prio {
+		return
+	}
+	// if current running priority is equal to this worker and there are still workers with the same priority waiting we should spin another worker with the same priority
+	if prio == curRunningPrio.Load() && prioWaitingCnt.get(prio) > 0 {
+		workerChans[prio] <- struct{}{}
+	} else { // there's no work being done at current or higher prio so we search in our queue for workers at lower priorities that are waiting
+		for i := prio - 1; i >= 0; i-- { // traverse priority queue downwards
+			if prioWaitingCnt.get(i) > 0 { // we find workers waiting at this prio so we unlock all of them
+				// set spinCnt to be the number of threads we have to spin; we check that it's within limits
+				spinCnt := Options.LowPrioSpinCnt
+				if spinCnt < 1 || prioWaitingCnt.get(i) < spinCnt {
+					spinCnt = prioWaitingCnt.get(i)
+				}
+				// update state
+				curRunningPrio.Store(i)
+				// send signals to workers
+				for j := int64(0); j < spinCnt; j++ {
+					workerChans[i] <- struct{}{}
+				}
+				// stop searching for lower priorities because we already do work at higher prio
+				break
+			}
+		}
+	}
 }
 
 func GetState() State {
-	getStateChan <- struct{}{}
-	return <-stateChan
+	state := State{
+		MaxEverRunningSamePrio: prioRunningCnt.getMaxEver(),
+		MaxEverWaitingSamePrio: prioWaitingCnt.getMaxEver(),
+	}
+	for i := int64(0); i < numPriorities; i++ {
+		state.RunningWorkersPerPrioCnt[i] = prioRunningCnt.get(i)
+		state.WaitingWorkersPerPrioCnt[i] = prioWaitingCnt.get(i)
+	}
+	return state
 }
 
-func workerCoordinator() {
-
-	var prioRunningCnt [100]int
-	var prioWaitingCnt [100]int
-	var curRunningPrio int
-
-	var maxEverWorkChanLen int
-	var maxEverRunningSamePrio int
-	var maxEverWaitingSamePrio int
-
-	for {
-		select {
-
-		// we receive signal worker needs to start at certain prio
-		case prio := <-workStartChan:
-			// if work prio is at least current running prio we can start immediately - no further processing or storing needs to be done
-			if prio >= curRunningPrio {
-				curRunningPrio = prio
-				prioRunningCnt[prio]++
-				workerChans[prio] <- struct{}{}
-			} else { // worker with higher priorities are running and current worker should wait (unless limit is exceeded)
-				// if the number of waiting threads hit the maxBlockedWorkers limit we let it run immediately; otherwise we add it to waiting queue
-				if Options.MaxBlockedWorkers > 0 && prioWaitingCnt[prio] >= Options.MaxBlockedWorkers {
-					prioRunningCnt[prio]++
-					workerChans[prio] <- struct{}{}
-				} else {
-					prioWaitingCnt[prio]++
-				}
-			}
-			// update stats
-			if len(workStartChan) > maxEverWorkChanLen {
-				maxEverWorkChanLen = len(workStartChan)
-			}
-			if prioWaitingCnt[prio] > maxEverWaitingSamePrio {
-				maxEverWaitingSamePrio = prioWaitingCnt[prio]
-			}
-
-		// we receive signal that worker with certain prio ended work
-		case prio := <-workEndChan:
-			prioRunningCnt[prio]--
-			// if current running priority is higher than this worker priority there's nothing else to do
-			if curRunningPrio > prio {
-				break
-			}
-			// if current running priority is equal to this worker and there are still workers with the same priority waiting we should spin another worker with the same priority
-			if prio == curRunningPrio && prioWaitingCnt[prio] > 0 {
-				prioRunningCnt[prio]++
-				workerChans[prio] <- struct{}{}
-			} else { // there's no work being done at current or higher prio so we search in our queue for workers at lower priorities that are waiting
-				for i := prio - 1; i >= 0; i-- { // traverse priority queue downwards
-					if prioWaitingCnt[i] > 0 { // we find workers waiting at this prio so we unlock all of them
-						// set spinCnt to be the number of threads we have to spin; we check that it's within limits
-						spinCnt := Options.LowPrioSpinCnt
-						if prioWaitingCnt[i] < spinCnt || spinCnt < 1 {
-							spinCnt = prioWaitingCnt[i]
-						}
-						// update state
-						curRunningPrio = i
-						prioWaitingCnt[i] -= spinCnt
-						prioRunningCnt[i] += spinCnt
-						// send signals to workers
-						for j := 0; j < spinCnt; j++ {
-							workerChans[i] <- struct{}{}
-						}
-						// stop searching for lower priorities because we already do work at higher prio
-						break
-					}
-				}
-			}
-			// update stats
-			if len(workEndChan) > maxEverWorkChanLen {
-				maxEverWorkChanLen = len(workEndChan)
-			}
-			if prioRunningCnt[prio] > maxEverRunningSamePrio {
-				maxEverRunningSamePrio = prioRunningCnt[prio]
-			}
-
-		// we receive signal that a client wants current state
-		case <-getStateChan:
-			state := State{
-				WorkStartChanLen:         len(workStartChan),
-				WorkEndChanLen:           len(workEndChan),
-				RunningWorkersPerPrioCnt: prioRunningCnt,
-				WaitingWorkersPerPrioCnt: prioWaitingCnt,
-				MaxEverRunningSamePrio:   maxEverRunningSamePrio,
-				MaxEverWaitingSamePrio:   maxEverWaitingSamePrio,
-			}
-			stateChan <- state
-
-		}
+func (counters *prioCounters) inc(prio int64) {
+	counters.prioCnt[prio].Add(1)
+	if counters.prioCnt[prio].Load() > counters.maxEver.Load() {
+		counters.maxEver.Store(counters.prioCnt[prio].Load())
 	}
+}
 
+func (counters *prioCounters) dec(prio int64) {
+	counters.prioCnt[prio].Add(-1)
+}
+
+func (counters *prioCounters) get(prio int64) int64 {
+	return counters.prioCnt[prio].Load()
+}
+
+func (counters *prioCounters) getMaxEver() int64 {
+	return counters.maxEver.Load()
 }
